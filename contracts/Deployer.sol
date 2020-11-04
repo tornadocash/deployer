e450e1befc30909aaf904cf7a16862637cf7ae53
poma <semenov.roma@gmail.com>
2020-11-04 05:39:04 +0300

    make deployer public

--- contracts/Deployer.sol
+++ contracts/Deployer.sol
@@ -6,7 +6,7 @@ interface IDeployer {
 }
 
 contract Deployer {
-  IDeployer immutable deployer;
+  IDeployer immutable public deployer;
 
   constructor(IDeployer _deployer) public {
     // Use EIP-2470 SingletonFactory address by default
